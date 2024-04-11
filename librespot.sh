0b7f71424f7cc30cd756ccd6045162d053898b19
RPi2User <gm.aier1905@gmail.com>
2024-04-11 18:54:32 +0200

    Initial Commit

---
+++ librespot.sh
@@ -0,0 +1,9 @@
+/root/.cargo/bin/librespot \
+    -n "Raspotify 3 (Rust)" \
+    --bitrate 320 \
+    --format F32 \
+    --system-cache /tmp/librespot_syscache \
+    --cache /tmp/librespot_contentcache \
+    --cache-size-limit 512M \
+    --device-type homething \
+    --volume-ctrl log
